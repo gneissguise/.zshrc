ad2074ea7048313c6e4cfaf5717df0533873e05a
Justin Frost <frostjust@gmail.com>
2021-08-01 16:23:47 -0700

    here we go

---
+++ .oh-my-zsh/custom/autoload_custom_plugs.zsh
@@ -0,0 +1,18 @@
+ # OMZ does not add nested comp dirs to fpath so do it here, assume *src has completions
+for plug in ${plugins[@]}; do
+    if [[ -d "$ZSH/custom/plugins/$plug" ]]; then
+    # null glob - no error
+        for dir in "$ZSH/custom/plugins/$plug/"*src(N); do
+            if [[ -d "$dir" ]]; then
+                if [[ -z ${fpath[(r)$dir]} ]];then
+                    if [[ $dir = *override* ]]; then
+                        fpath=($dir $fpath)
+                    else
+                        fpath=($fpath $dir)
+                    fi
+                # echo "add $dir to $fpath" >> "$ZPWR_LOGFILE"
+                fi
+            fi
+        done
+    fi
+done
---
+++ .oh-my-zsh/custom/completions.zsh
@@ -0,0 +1,5 @@
+
+   #Completions
+   [ -s "/home/frostjust/.jabba/jabba.sh" ] && source "/home/frostjust/.jabba/jabba.sh"
+   [ -s $HOME/.asdf/asdf.sh ] && source $HOME/.asdf/asdf.sh    
+   [ -s $HOME/.asdf/completions/asdf.bash ] && source $HOME/.asdf/completions/asdf.bash
